083fa50fb5041463b88f71303fdb19aac4508af9
Ayush Deolasee <pypdeveloper@deolasee.org>
2026-03-23 22:30:31 +0530

    adding helper functions in lr.py

--- llm/lr.py
+++ llm/lr.py
@@ -9,3 +9,14 @@ def get_lr(it, warmup_steps, max_lr, max_steps, min_lr):
     assert 0 <= decay_ratio <= 1
     coeff = 0.5 * (1.0 + math.cos(math.pi * decay_ratio)) # coeff starts at 1 and goes to 0
     return min_lr + coeff * (max_lr - min_lr)
+
+def get_muon_momentum(it):
+    frac = min(it / 300, 1)
+    momentum = (1 - frac) * 0.85 + frac * 0.95
+    return momentum
+
+# Weight decay scheduler for Muon optimizer (linearly decays to zero over the course of training)
+def get_weight_decay(it):
+    return weight_decay_scaled * (1 - it / num_iterations)
+
+
